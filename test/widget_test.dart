52bfbf7c302f6d9d208c45394cf6795c7d8ce340
Matthew Christopher Albert <matthewmcavaio@gmail.com>
2022-06-07 23:42:59 +0700

    feat: change app name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_beginner/main.dart';
+import 'package:mocation/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
